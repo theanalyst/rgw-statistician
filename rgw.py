6fe23122ff2c7d4b79d4c10b03be5c9b79352904
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2015-01-06 18:13:47 +0530

    adding an RGWAdminOp class

--- rgw.py
+++ rgw.py
@@ -1,3 +1,8 @@
+import requests
+from awsauth import S3Auth
+from collections import namedtuple
+
+
 class RGWStats(object):
     def __init__(self, user_id):
         """
@@ -9,3 +14,35 @@ class RGWStats(object):
         self.uid = user_id
         self.buckets = []
 
+
+class RGWAdminOp(object):
+    def __init__(self, host, access_key, secret, secure=True):
+        """
+        Basic class holding the admin details
+        """
+        self.access_key = access_key
+        self.secret = secret
+        self.host = host
+        self.protocol = "https" if secure else "http"
+        self.endpoint = "{protocol}//{host}/admin".format(self.protocol,
+                                                          host=host)
+
+    def get_bucket_stats(self, tenant_id):
+        METHOD = "bucket"
+        r = requests.get("{endpoint}/{method}".format(self.endpoint, METHOD),
+                         params={"uid": tenant_id, "stats": True},
+                         auth=S3Auth(self.access_key, self.secret, self.host)
+                         )
+
+        stats = RGWStats(tenant_id)
+        stats.buckets = [b for b in self.iter_bucket_stats(r.json())]
+
+    @staticmethod
+    def iter_bucket_stats(json_data):
+        # TODO: handle failures
+        Bucket = namedtuple('Bucket', 'name, num_objects, size')
+        for it in json_data:
+            for k, v in it["usage"].items():
+                yield Bucket(it["bucket"], v["num_objects"], v["size_kb"])
+            else:
+                yield Bucket(it["bucket"], 0, 0)
